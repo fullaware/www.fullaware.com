104c9c6169c6e9910aabb7d73923cc97749e7a28
Full Aware <fullaware@@users.noreply.github.com>
2024-12-29 09:54:36 -0500

    RM AI

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734921717',resources: [
+    cacheName: 'chirpy-1735484047',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
