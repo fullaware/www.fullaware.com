d8f07af0b9e14fcbc2b4c90acf8aebdd0c7af0a5
Full Aware <fullaware@@users.noreply.github.com>
2025-03-09 20:48:10 -0400

    Updated Asteroid Mining with LLMs

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741567318',resources: [
+    cacheName: 'chirpy-1741567645',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
