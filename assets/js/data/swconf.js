f94703cf5bd70d76d77c6e65cbe2ca59b5b54006
Full Aware <fullaware@@users.noreply.github.com>
2025-03-18 20:34:43 -0400

    Updated postings

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742344342',resources: [
+    cacheName: 'chirpy-1742344482',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
--- publish.sh
+++ publish.sh
@@ -3,6 +3,7 @@ git commit -m "Updated postings"
 git push
 bundle exec jekyll build --destination ../www.fullaware.com/
 cd ../www.fullaware.com/
+git pull
 git add -A
 git commit -m "Updated postings"
 git push
