6fe06d98f2a4761b9b2ed2230c9b88d70fd5a27d
Full Aware <fullaware@@users.noreply.github.com>
2025-03-24 19:03:56 -0400

    Updated postings

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742844481',resources: [
+    cacheName: 'chirpy-1742857435',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
