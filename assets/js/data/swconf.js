7e6a83ecb90ae95373c37ffe098351fc9e19b3cf
Full Aware <fullaware@users.noreply.github.com>
2026-06-21 20:46:55 -0400

    Added Loop Library link to article

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1782088636',resources: [
+    cacheName: 'chirpy-1782089214',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
